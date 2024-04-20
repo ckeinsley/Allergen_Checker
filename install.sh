f49f050d8619c45751eaeca13f85895430db7c0c
Christopher Keinsley <keinsleychris@gmail.com>
2024-04-20 10:41:21 -0500

    trying to build flutter app

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#######################################################
+#  This is set up to install the AllergenCheckerApp   #
+#######################################################
+
+sudo cp -r Allergen_Checker/AllergenCheckerApp/build/web/ /var/www/html/
